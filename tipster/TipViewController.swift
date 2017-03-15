b392f46304e46eb33b51da7136b12d2022ba2826
Carroll Yu <carrollcyu@gmail.com>
2017-03-14 17:02:18 -0700

    improvements

--- tipster/TipViewController.swift
+++ tipster/TipViewController.swift
@@ -20,7 +20,17 @@ class TipViewController: UIViewController {
 //        print("tip view did load")
         let defaults = UserDefaults.standard
         tipChoice.selectedSegmentIndex = defaults.integer(forKey: "tipChoice")
-        print(tipChoice.selectedSegmentIndex)
+
+        let currentTime = NSDate()
+        let lastUpdated = defaults.object(forKey: "lastUpdated") ?? currentTime
+        let billAmount = defaults.object(forKey: "billAmount") ?? 0
+        let interval = currentTime.timeIntervalSince(lastUpdated as! Date)
+
+        // Use last bill amount if it was updated within the last 10 minutes
+        if (interval < 600) {
+            subtotalField.text = String(describing: billAmount)
+            calculateTip(self)
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -52,15 +62,22 @@ class TipViewController: UIViewController {
         view.endEditing(true)
     }
     
+    @IBAction func updateBillAmount(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        let currentTime = NSDate()
+        defaults.set(currentTime, forKey: "lastUpdated")
+        defaults.set(subtotalField.text, forKey: "billAmount")
+        defaults.synchronize()
+    }
+    
     @IBAction func calculateTip(_ sender: AnyObject) {
         let tipPercentages = [0.1, 0.15, 0.2]
         let subtotal = Double(subtotalField.text!) ?? 0
-        let tip = subtotal * tipPercentages[tipChoice.selectedSegmentIndex]
-        let total = subtotal + tip
+        let tip = Double(subtotal * tipPercentages[tipChoice.selectedSegmentIndex])
+        let total = Double(subtotal + tip)
             
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
     
 }
-
